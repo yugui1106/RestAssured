70ebb562fcb96a4e31a77710b1743f11bb2dcf0e
chengzw <chengzw@dxy.cn>
2018-08-22 18:57:23 +0800

    change

--- src/test/java/ClassTest.java
+++ src/test/java/ClassTest.java
@@ -18,8 +18,8 @@ public class ClassTest {
     @Test
     public void testLogin(){
         Map<String, Object> jsonMap = new HashMap<String, Object>();
-        jsonMap.put("u", "18268130294");
-        jsonMap.put("p", "dxy123456");
+        jsonMap.put("u", "");
+        jsonMap.put("p", "");
         jsonMap.put("ac", "4124c5f1-1031-4fda-b06f-a88ac5ad8d9f");
         jsonMap.put("mc", "8d9576f998bfae4ea6f34d236fec7596ec41e286");
         Response res =
